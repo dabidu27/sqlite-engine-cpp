34fd219f21e0282e38363b967a3ef3cf006e1929
david <adamita.david@gmail.com>
2025-11-28 23:23:12 +0200

    Added a smarter tokenizer and updated select regex pattern

--- sqlite engine/StringUtils.cpp
+++ sqlite engine/StringUtils.cpp
@@ -1,7 +1,7 @@
 #include "StringUtils.h"
 
 
-void toUpper(std::string& command) {
+void StringUtils::toUpper(std::string& command) {
 
 	for (int i = 0; i < command.length(); i++)
 		if (command[i] >= 'a' && command[i] <= 'z')
