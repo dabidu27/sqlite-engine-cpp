27852e85a45ecb9824bc0c0bfb5a23b1c75ea9bc
david <adamita.david@gmail.com>
2025-12-09 01:55:29 +0200

    Worked on writing and reading a binary file for permanent storage of table metadata

--- sqlite engine/test.cpp
+++ sqlite engine/test.cpp
@@ -1,10 +1,11 @@
-
+#include <fstream>
 #include <iostream>
 #include "CommandParser.h"
 #include "CommandType.h"
 #include "Columns.h"
 #include "Table.h"
 #include "Database.h"
+
 using namespace std;
 
 int main() {
@@ -25,6 +26,24 @@ int main() {
 		parser.runCommand(db);
 		cout << endl << "Now DB looks like this: " << endl;
 		cout << db;
+	}
+
+	ofstream writeFile("table_metadata.bin", ios::binary);
+	if (writeFile.is_open()) {
+		db.writeTabelsMetadata(writeFile);
+		writeFile.close();
+	}
+	else {
+		throw "Couldn't create/open file";
+	}
+
+	ifstream readFile("table_metadata.bin", ios::binary);
+	if (readFile.is_open()) {
+		db.printReadTablesMetadata(readFile);
+		readFile.close();
+	}
+	else {
+		throw "Couldn't open file";
 	}
 
 	return 0;
